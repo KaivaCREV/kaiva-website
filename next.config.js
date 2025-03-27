64a7c9d2cf493bbc79e82c956bcb93d0b40e2242
Veraldi 0707 <kaivajoev@gmail.com>
2025-03-27 13:30:49 -0700

    Trigger redeploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['localhost'],
+    unoptimized: true,
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
